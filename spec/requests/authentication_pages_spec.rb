3777e359db8f8afe5012b21be00a2aed57965234
hung.nguyenhoang <hung.nguyen.2@minerva.kgi.edu>
2019-06-14 08:50:29 +0700

    added authentication tests

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -22,25 +22,35 @@ describe "Authentication" do
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
-      before do
-        fill_in "Email",    with: user.email.upcase
-        fill_in "Password", with: user.password
-        click_button "Sign in"
+
+      describe "with signing in" do
+        before do
+          fill_in "Email",    with: user.email.upcase
+          fill_in "Password", with: user.password
+          click_button "Sign in"
+        end
+
+        it { should have_title(user.name) }
+        it { should have_link('Users',       href: users_path) }
+        it { should have_link('Profile',     href: user_path(user)) }
+        it { should have_link('Settings',    href: edit_user_path(user)) }
+        it { should have_link('Sign out',    href: signout_path) }
+        it { should_not have_link('Sign in', href: signin_path) }
+
+        describe "followed by signout" do
+          before { click_link "Sign out" }
+          it { should have_link('Sign in') }
+        end
       end
-
-      it { should have_title(user.name) }
-      it { should have_link('Users',       href: users_path) }
-      it { should have_link('Profile',     href: user_path(user)) }
-      it { should have_link('Settings',    href: edit_user_path(user)) }
-      it { should have_link('Sign out',    href: signout_path) }
-      it { should_not have_link('Sign in', href: signin_path) }
-
-      describe "followed by signout" do
-        before { click_link "Sign out" }
-        it { should have_link('Sign in') }
+      
+      describe "without signing in" do
+        it { should_not have_link('Users',       href: users_path) }
+        it { should_not have_link('Profile',     href: user_path(user)) }
+        it { should_not have_link('Settings',    href: edit_user_path(user)) }
+        it { should_not have_link('Sign out',    href: signout_path) }
+        it { should have_link('Sign in', href: signin_path) }
       end
     end
-
   end
 
   describe "authorization" do
@@ -111,6 +121,6 @@ describe "Authentication" do
         specify { expect(response).to redirect_to(root_url) }
       end
     end
-    
+
   end
 end
